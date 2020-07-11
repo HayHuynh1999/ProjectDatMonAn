5fd5e0723ae06d34ae676ae15546243ee26053f3
Tran Thanh Tung <44186307+daimaou69@users.noreply.github.com>
2020-07-11 22:44:36 +0700

    Delete AdminActivity and StoreActivity :V

--- app/src/main/java/com/example/projectdatmonanonline/AdminActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.projectdatmonanonline;
-
-public class AdminActivity {
-    //Tao Admin
-}
--- app/src/main/java/com/example/projectdatmonanonline/StoreActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.projectdatmonanonline;
-
-public class StoreActivity {
-    //Tao Store
-}
